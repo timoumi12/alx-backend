3fb816fedcdea3aad2ab8dd590d6730ade0f25da
iheb <ihebtimoumi12@gmail.com>
2024-02-18 09:40:01 +0100

    hot fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+'''task0'''
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 
 @app.route('/')
 def index():
+    '''returns 0-index.html'''
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=0.0.0.0, port=5000, debug=True)
